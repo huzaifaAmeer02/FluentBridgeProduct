9114b4c49e4988f4d90dbc8201230b0198656cde
HuzaifaAmeer <huzaifaameer00@gmail.com>
2024-02-06 22:03:39 +0530

    game-notdone-about-bot

--- src/components/ActivityPage/ActivityPanel.jsx
+++ src/components/ActivityPage/ActivityPanel.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSpring, animated } from "react-spring";
 import VocabPanel from "../VocabPanel/vocabPanel.jsx";
 import imageBG from "../../assets/buttonBG.jpg";
+import ChatBotModal from "../ChatBotModal/ChatBotModal.jsx";
+import { FiMessageSquare } from "react-icons/fi"; // Importing the Message Square icon from react-icons
 
 const ActivityPanel = () => {
-    // Define entrance animations using react-spring
+    const [showChatBot, setShowChatBot] = useState(false);
+
     const fadeIn = useSpring({
         opacity: 1,
         from: { opacity: 0 },
-        config: { duration: 800 }, // Adjust the duration as needed
+        config: { duration: 800 },
     });
 
     const slideIn = useSpring({
         transform: "translateX(0)",
         from: { transform: "translateX(-100%)" },
-        config: { duration: 800 }, // Adjust the duration as needed
+        config: { duration: 800 },
     });
 
-    // Define bouncing animation for buttons
     const bounceIn = useSpring({
         opacity: 1,
         transform: "translateY(0)",
         from: { opacity: 0, transform: "translateY(-100%)" },
-        config: { duration: 800, delay: 300, tension: 300, friction: 10 }, // Adjust the duration, delay, tension, and friction as needed
+        config: { duration: 800, delay: 300, tension: 300, friction: 10 },
     });
+
+    const chatBotButtonStyle = {
+        backgroundColor: "#023C41",
+    };
 
     const panelStyle = {
         backgroundImage: `url(${imageBG})`,
@@ -32,8 +38,21 @@ const ActivityPanel = () => {
         backgroundPosition: "center",
     };
 
+    const toggleChatBot = () => {
+        setShowChatBot(!showChatBot);
+    };
+
     return (
-        <div className="flex flex-col md:flex-row h-screen" style={panelStyle}>
+        <div className="relative">
+            {/* ChatBot Button */}
+            <animated.div
+                className="fixed bottom-8 right-8 bg-blue-500 hover:bg-blue-700 text-white p-3 text-center transition rounded-full z-10"
+                style={fadeIn}
+                onClick={toggleChatBot}
+            >
+                <FiMessageSquare />
+            </animated.div>
+
             {/* Activity Panel */}
             <animated.div
                 style={{ ...slideIn, background: "rgba(255, 255, 255, 0.8)" }}
@@ -87,6 +106,9 @@ const ActivityPanel = () => {
 
             {/* Vocab Panel */}
             <VocabPanel className="md:flex-grow overflow-y-auto" />
+
+            {/* ChatBot Modal */}
+            {showChatBot && <ChatBotModal onClose={toggleChatBot} />}
         </div>
     );
 };
