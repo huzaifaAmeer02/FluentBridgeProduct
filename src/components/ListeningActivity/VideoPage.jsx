448508421c5aeeded5ca701fda050fc973cadb67
HuzaifaAmeer <huzaifaameer00@gmail.com>
2024-02-12 17:21:46 +0530

    listening-activity-1.3.3

--- src/components/ListeningActivity/VideoPage.jsx
+++ src/components/ListeningActivity/VideoPage.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -17,5 +18,4 @@ const VideoPage = ({ onClose, videoUrl }) => {
         </div>
     );
 };
-
 export default VideoPage;
