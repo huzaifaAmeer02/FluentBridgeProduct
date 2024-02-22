14edc91fa1e4f4ae6cd9b3d74755aef2932ae581
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-02-22 22:03:21 +0530

    vocabpanel-1.7.1

--- FluentBridge-Client/src/components/VocabPanel/vocabPanel.jsx
+++ FluentBridge-Client/src/components/VocabPanel/vocabPanel.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
+import vocabback from "../../assets/vocabback.jpg"
 
 const VocabPanel = () => {
+    const containerStyle = {
+        backgroundImage: `url(${vocabback})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
+
     return (
-        <div className="p-8">
+        <div className="p-8" style={containerStyle}>
             <Link
                 to="/activities"
                 className="back-to-activities flex items-center text-blue-500 font-bold hover:text-blue-700 transition duration-300 ease-in-out rounded-lg p-2 bg-white absolute left-4 top-4"
@@ -12,16 +24,16 @@ const VocabPanel = () => {
                 <IoIosArrowBack />
             </Link>
 
-            <div className="flex justify-between mt-8">
+            <div className="flex justify-center mt-8">
                 <Link
                     to="/dictionary"
-                    className="button1 flex items-center justify-center text-white font-bold py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out"
+                    className="button1 flex items-center justify-center text-white font-bold py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out mx-2"
                 >
                     Dictionary
                 </Link>
                 <Link
                     to="/vocabulary-activity"
-                    className="button2 flex items-center justify-center text-white font-bold py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out"
+                    className="button2 flex items-center justify-center text-white font-bold py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out mx-2"
                 >
                     Vocabulary Activity
                 </Link>
