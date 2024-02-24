617bcb23216c020c0f40a70982316ffd8bf4ab47
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-02-24 17:03:59 +0530

    GradingAssesment-1.8.6-2-Next

--- FluentBridge-Client/src/main.jsx
+++ FluentBridge-Client/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import store from "./Redux/Store.js";
+import { Provider } from "react-redux";
 
-// checking faslans git update
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
