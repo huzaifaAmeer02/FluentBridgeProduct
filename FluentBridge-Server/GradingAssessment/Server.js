69a4f447e88c47ddcdbfed2283625d256fd67d91
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-02-28 13:51:56 +0530

    GradingAssesment-2.0.1-Server-2

--- FluentBridge-Server/GradingAssessment/Server.js
+++ FluentBridge-Server/GradingAssessment/Server.js
@@ -1,12 +1,25 @@
-import express from "express"
+import express from "express";
+import morgan from "morgan"
+import cors from "cors"
+import {config} from "dotenv";
 
-const app = express
+const app = express(); // Call the express function to create an instance of the application
+
+/*middle ware*/
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
 
 /*routes*/
-app.get("/gradingquiz",(req, res) => {
+app.get("/", (req, res) => {
     try {
-        res.json("Get Request")
-    }catch (error){
-        res.json(error)
+        res.json("Get Request");
+    } catch (error) {
+        res.json(error);
     }
-})
+});
+
+app.listen(3000, () => {
+    console.log("Server Connected to http://localhost:3000");
+});
+
