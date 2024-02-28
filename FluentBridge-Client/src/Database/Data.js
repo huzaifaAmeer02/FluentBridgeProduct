96ed754af70a529234ff91026eba33e6601ed625
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-02-28 12:52:40 +0530

    GradingAssesment-2.0.1-Server-1.1

--- FluentBridge-Client/src/Database/Data.js
+++ FluentBridge-Client/src/Database/Data.js
@@ -1,13 +1,14 @@
 export default [
     {
         id:1,
-        questions : "1st Question is here",
+        questions : "\n" +
+            "In this heartfelt narrative, Dananjaya Hettiarachchi shares a poignant journey of resilience, self-discovery, and the transformative power of human connections. Despite facing challenges and setbacks, Dananjaya finds solace and guidance from his parents, mentors, and his loving wife, who see potential in him even when he feels lost. Through their unwavering support and belief in his abilities, Dananjaya learns to embrace his uniqueness, pursue his dreams, and ultimately thrive as a speaker and educator. His story serves as a powerful reminder that amidst life's trials, the kindness and encouragement of others can uplift and empower us to overcome obstacles and fulfill our potential. What is the primary message conveyed by Dananjaya Hettiarachchi's story?",
         video : "https://youtu.be/RCXzH27eOIA?list=RDRCXzH27eOIA",
         options : [
-            "Answer 1",
-            "Answer 2",
-            "Answer 3",
-            "Answer 4",
+            "The importance of financial stability in achieving success.",
+            "The significance of perseverance and resilience in overcoming adversity.",
+            "The necessity of academic achievements for personal growth.",
+            "The role of luck in determining one's life trajectory.",
         ]
     },
     {
@@ -23,13 +24,14 @@ export default [
     },
     {
         id:3,
-        questions : "3rd Question is here",
+        questions : "\n" +
+            "In a world where chaos reigns supreme, Bob found himself in a rather peculiar predicament. While attempting to bake a cake for his neighbor's birthday, Bob accidentally mistook salt for sugar, turning his supposedly sweet confection into a savory disaster. As if that weren't enough, Bob's misadventures continued when he attempted to ice the cake, only to realize he had used mayonnaise instead of frosting! With a shrug and a nervous chuckle, Bob dubbed his creation the \"Salt & Mayo Surprise.\" Amidst the laughter and puzzled expressions from his guests, one burning question lingered in everyone's minds: What made Bob's cake a truly unique culinary experience?",
         video : "",
         options : [
-            "Answer 1",
-            "Answer 2",
-            "Answer 3",
-            "Answer 4",
+            "Chocolate chips and pickles",
+            "Peanut butter and anchovies",
+            "Salt and mayonnaise",
+            "Strawberry jam and ketchup",
         ]
     },
     {
