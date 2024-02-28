b558bbeb9f01a38de9680ab587fafeffcc507bee
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-02-28 14:14:30 +0530

    updated 2.1

--- FluentBridge-Client/src/components/ChatBotModal/Title.jsx
+++ FluentBridge-Client/src/components/ChatBotModal/Title.jsx
@@ -36,7 +36,7 @@ const Title = ({ setMessages }) => {
 
     return (
         <div className="flex justify-between items-center w-full p-4 bg-gray-900 text-white font-bold shadow">
-            <div className="italic">Rachel</div>
+            <div className="italic">FluentBridge</div>
             <button
                 onClick={resetConversation}
                 className={
