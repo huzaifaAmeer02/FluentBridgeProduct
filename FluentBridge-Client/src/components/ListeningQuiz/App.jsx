8cbd787f453fcc7b8b8cc5a74e0cd75ad251efc0
Muhammadu Ishrath <ishrathmuhammadu@gmail.com>
2024-02-29 23:08:46 +0530

    Added a Result panel
    Added a Result Panel Without Responsiveness  and a Back Button

--- FluentBridge-Client/src/components/ListeningQuiz/App.jsx
+++ FluentBridge-Client/src/components/ListeningQuiz/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
 import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
 import Start from "./components/Start";
 import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
+import WinningPanel from "./components/WinningPanel";
+import { IoIosArrowBack } from "react-icons/io";
+
 
 function App() {
   const [username, setUsername] = useState(null);
   const [timeOut, setTimeOut] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(1);
   const [earned, setEarned] = useState("$ 0");
+  const [quizCompleted, setQuizCompleted] = useState(false);
 
   const data = [
     {
@@ -55,292 +60,7 @@ function App() {
         },
       ],
     },
-//     {
-//       id: 3,
-//       question: "Why does William have back pain?",
-//       answers: [
-//         {
-//           text: "Unknown cause",
-//           correct: false,
-//         },
-//         {
-//           text: "Running injury",
-//           correct: false,
-//         },
-//         {
-//           text: " Car accident",
-//           correct: false,
-//         },
-//         {
-//           text: "Bike accident",
-//           correct: true,
-//         },
-//       ],
-//     },
-//         {
-//           id: 4,
-//           question: "Where specifically does William feel the pain in his back?",
-//           answers: [
-//             {
-//               text: "Upper back",
-//               correct: false,
-//             },
-//             {
-//               text: "Middle back",
-//               correct: false,
-//             },
-//             {
-//               text: "Lower back",
-//               correct: true,
-//             },
-//             {
-//               text: "Entire back",
-//               correct: false,
-//             },
-//           ],
-//         },
-//             {
-//               id: 5,
-//               question: "Where specifically does William feel the pain in his back?",
-//               answers: [
-//                 {
-//                   text: "Upper back",
-//                   correct: false,
-//                 },
-//                 {
-//                   text: "Middle back",
-//                   correct: false,
-//                 },
-//                 {
-//                   text: "Lower back",
-//                   correct: true,
-//                 },
-//                 {
-//                   text: "Entire back",
-//                   correct: false,
-//                 },
-//               ],
-//             },
-//             {
-//               id: 6,
-//               question: "Where specifically does William feel the pain in his back?",
-//               answers: [
-//                 {
-//                   text: "Upper back",
-//                   correct: false,
-//                 },
-//                 {
-//                   text: "Middle back",
-//                   correct: false,
-//                 },
-//                 {
-//                   text: "Lower back",
-//                   correct: true,
-//                 },
-//                 {
-//                   text: "Entire back",
-//                   correct: false,
-//                 },
-//               ],
-//             },
-//             {
-//               id: 7,
-//               question: "Where specifically does William feel the pain in his back?",
-//               answers: [
-//                 {
-//                   text: "Upper back",
-//                   correct: false,
-//                 },
-//                 {
-//                   text: "Middle back",
-//                   correct: false,
-//                 },
-//                 {
-//                   text: "Lower back",
-//                   correct: true,
-//                 },
-//                 {
-//                   text: "Entire back",
-//                   correct: false,
-//                 },
-//               ],
-//             },
-//                 {
-//                   id: 8,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 9,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 10,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 11,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 12,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 13,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 14,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
-//                 {
-//                   id: 15,
-//                   question: "Where specifically does William feel the pain in his back?",
-//                   answers: [
-//                     {
-//                       text: "Upper back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Middle back",
-//                       correct: false,
-//                     },
-//                     {
-//                       text: "Lower back",
-//                       correct: true,
-//                     },
-//                     {
-//                       text: "Entire back",
-//                       correct: false,
-//                     },
-//                   ],
-//                 },
+
   ];
 
   const moneyPyramid = useMemo(
@@ -367,63 +87,68 @@ function App() {
 
   useEffect(() => {
     questionNumber > 1 &&
-    setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
   }, [questionNumber, moneyPyramid]);
 
   const handleStart = () => {
     setUsername("Guest");
   };
 
+
+
   return (
-      <div className="app">
-        {!username ? (
-            <Start onStart={handleStart} />
-        ) : (
-            <>
-              <div className="main">
-                {timeOut ? (
-                    <h1 className="endText">You earned: {earned}</h1>
-                ) : (
-                    <>
-                      <div className="top">
-                        <div className="timer">
-                          <Timer
-                              setTimeOut={setTimeOut}
-                              questionNumber={questionNumber}
-                          />
-                        </div>
-                      </div>
-                      <div className="bottom">
-                        <Trivia
-                            data={data}
-                            questionNumber={questionNumber}
-                            setQuestionNumber={setQuestionNumber}
-                            setTimeOut={setTimeOut}
-                        />
-                      </div>
-                    </>
-                )}
-              </div>
-              <div className="pyramid">
-                <ul className="moneyList">
-                  {moneyPyramid.map((m) => (
-                      <li
-                          className={
-                            questionNumber === m.id
-                                ? "moneyListItem active"
-                                : "moneyListItem"
-                          }
-                          key={m.id}
-                      >
-                        <span className="moneyListItemNumber">{m.id}</span>
-                        <span className="moneyListItemAmount">{m.amount}</span>
-                      </li>
-                  ))}
-                </ul>
-              </div>
-            </>
-        )}
-      </div>
+    <div className="app">
+      <Link to="/listening" className="back-to-activities flex items-center text-blue-500 font-bold hover:text-blue-700 transition duration-300 ease-in-out rounded-lg p-2 bg-white absolute left-4 top-4">
+        <IoIosArrowBack />
+      </Link>
+      {!username ? (
+        <Start onStart={handleStart} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut || quizCompleted ? (
+              <>
+                <WinningPanel earned={earned} />
+              </>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setTimeOut={setTimeOut} questionNumber={questionNumber} />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                    onQuizComplete={() => setQuizCompleted(true)} // Pass callback to Trivia component
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          {!timeOut && !quizCompleted && (
+            <div className="pyramid">
+              <ul className="moneyList">
+                {moneyPyramid.map((m) => (
+                  <li
+                    className={
+                      questionNumber === m.id ? "moneyListItem active" : "moneyListItem"
+                    }
+                    key={m.id}
+                  >
+                    <span className="moneyListItemNumber">{m.id}</span>
+                    <span className="moneyListItemAmount">{m.amount}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </>
+      )}
+    </div>
   );
 }
 
