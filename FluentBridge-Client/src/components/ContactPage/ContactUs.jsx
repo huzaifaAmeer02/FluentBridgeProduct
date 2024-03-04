54000c36cd8917c00f6bac3650b7a135ebfe2d8d
nuwafazley <azleynuwaf2002@gmail.com>
2024-03-05 00:02:37 +0530

    contact-form

--- FluentBridge-Client/src/components/ContactPage/ContactUs.jsx
+++ FluentBridge-Client/src/components/ContactPage/ContactUs.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react"
 
 const ContactUs = () => {
@@ -5,6 +6,7 @@ const ContactUs = () => {
         <>
             <section className='hero'>
                 <h1>Welcome To Contact Page</h1>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae distinctio laboriosam numquam quisquam. Blanditiis consequuntur illum perspiciatis quos ullam, voluptatem!</div>
             </section>
         </>
     )
