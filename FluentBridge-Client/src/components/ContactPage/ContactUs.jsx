aa694c94769f68cd5718f7104e5705178bdcd870
nuwafazley <azleynuwaf2002@gmail.com>
2024-03-05 00:22:51 +0530

    contact-update-02

--- FluentBridge-Client/src/components/ContactPage/ContactUs.jsx
+++ FluentBridge-Client/src/components/ContactPage/ContactUs.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react"
+
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -30,12 +31,145 @@ const ContactUs = () => {
         setState({ ...state, [name]: value });
     };
     return (
-        <>
-            <section className='hero'>
-                <h1>Welcome To Contact Page</h1>
-                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae distinctio laboriosam numquam quisquam. Blanditiis consequuntur illum perspiciatis quos ullam, voluptatem!</div>
-            </section>
-        </>
+        <section className="contact-section">
+            <div className="container">
+                <ToastContainer position="top-center" />
+                <div className="row justify-content-center">
+                    <div className="col-md-10">
+                        <div className="wrapper">
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                    <div className="contact-wrap w-100 p-lg-5 p-4">
+                                        <h3 className="mb-4">Send us a message</h3>
+                                        <form
+                                            id="contactForm"
+                                            className="contactForm"
+                                            onSubmit={handleSubmit}
+                                        >
+                                            <div className="row">
+                                                <div className="col-md-12">
+                                                    <div className="form-group">
+                                                        <input
+                                                            type="text"
+                                                            className="form-control"
+                                                            name="name"
+                                                            placeholder="Name"
+                                                            onChange={handleInputChange}
+                                                            value={name}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-12">
+                                                    <div className="form-group">
+                                                        <input
+                                                            type="email"
+                                                            className="form-control"
+                                                            name="email"
+                                                            placeholder="Email"
+                                                            onChange={handleInputChange}
+                                                            value={email}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-12">
+                                                    <div className="form-group">
+                                                        <input
+                                                            type="text"
+                                                            className="form-control"
+                                                            name="subject"
+                                                            placeholder="Subject"
+                                                            onChange={handleInputChange}
+                                                            value={subject}
+                                                        />
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-12">
+                                                    <div className="form-group">
+                            <textarea
+                                type="text"
+                                className="form-control"
+                                name="message"
+                                placeholder="Message"
+                                cols="30"
+                                rows="6"
+                                onChange={handleInputChange}
+                                value={message}
+                            ></textarea>
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-12">
+                                                    <div className="form-group">
+                                                        <input
+                                                            type="submit"
+                                                            value="Send Message"
+                                                            className="btn btn-primary"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 d-flex align-items-stretch">
+                                    <div className="info-wrap w-100 p-lg-5 p-4 img">
+                                        <h3>Contact us</h3>
+                                        <p className="mb-4">
+                                        </p>
+                                        <div className="dbox w-100 d-flex align-items-start">
+                                            <div className="icon d-flex align-items-center justify-content-center">
+                                                <span className="fa fa-map-marker"></span>
+                                            </div>
+                                            <div className="text pl-3">
+                                                <p>
+                                                    <span>Address:</span> Colombo Srilanka
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="dbox w-100 d-flex align-items-center">
+                                            <div className="icon d-flex align-items-center justify-content-center">
+                                                <span className="fa fa-phone"></span>
+                                            </div>
+                                            <div className="text pl-3">
+                                                <p>
+                                                    <span>Phone:</span>
+                                                    <a href="tel://123456789">+94 774322192</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="dbox w-100 d-flex align-items-center">
+                                            <div className="icon d-flex align-items-center justify-content-center">
+                                                <span className="fa fa-paper-plane"></span>
+                                            </div>
+                                            <div className="text pl-3">
+                                                <p>
+                                                    <span>Email:</span>
+                                                    <a href="mailto:info@yoursite.com">
+                                                        info@yoursite.com
+                                                    </a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="dbox w-100 d-flex align-items-center">
+                                            <div className="icon d-flex align-items-center justify-content-center">
+                                                <span className="fa fa-globe"></span>
+                                            </div>
+                                            <div className="text pl-3">
+                                                <p>
+                                                    <span>Website:</span>
+                                                    <a href="#">yoursite.com</a>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
+
+
 }
 export default ContactUs
