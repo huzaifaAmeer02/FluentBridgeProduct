9d9925563e73ada2f0e6e825381abbbd34fbda48
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-04 22:04:46 +0530

    updated

--- FluentBridge-Client/src/components/ChatBotModal/Title.jsx
+++ FluentBridge-Client/src/components/ChatBotModal/Title.jsx
@@ -1,126 +1,68 @@
 import { useState } from "react";
-import Title from "./Title";
 import axios from "axios";
-import RecordMessage from "./RecordMessage";
 
-const Controller = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [messages, setMessages] = useState([]);
+/*
+type Props = {
+    setMessages: any
+};
+*/
 
-    function createBlobURL(data) {
-        const blob = new Blob([data], { type: "audio/mpeg" });
-        const url = window.URL.createObjectURL(blob);
-        return url;
-    }
 
-    const handleStop = async (blobUrl) => {
-        setIsLoading(true);
 
-        // Append recorded message to messages
-        const myMessage = { sender: "me", blobUrl };
-        const messagesArr = [...messages, myMessage];
 
-        // convert blob url to blob object
-        fetch(blobUrl)
-            .then((res) => res.blob())
-            .then(async (blob) => {
-                // Construct audio to send file
-                const formData = new FormData();
-                formData.append("file", blob, "myFile.wav");
 
-                // send form data to api endpoint
-                await axios
-                    .post("http://localhost:8000/post-audio", formData, {
-                        headers: {
-                            "Content-Type": "audio/mpeg",
-                        },
-                        responseType: "arraybuffer", // Set the response type to handle binary data
-                    })
-                    .then((res) => {
-                        const blob = res.data;
-                        const audio = new Audio();
-                        audio.src = createBlobURL(blob);
+const Title = ({ setMessages }) => {
+    const [isResetting, setIsResetting] = useState(false);
 
-                        // Append to audio
-                        const rachelMessage = { sender: "rachel", blobUrl: audio.src };
-                        messagesArr.push(rachelMessage);
-                        setMessages(messagesArr);
+    // Reset conversation
+    const resetConversation = async () => {
+        setIsResetting(true);
 
-                        // Play audio
-                        setIsLoading(false);
-                        audio.play();
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                        setIsLoading(false);
-                    });
-            });
+        await axios
+            .get("http://localhost:8000/reset", {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then((res) => {
+                if (res.status === 200) {
+                    setMessages([]);
+                }
+            })
+            .catch((err) => {});
+
+        setIsResetting(false);
     };
 
     return (
-        <div className="h-screen overflow-y-hidden">
-            {/* Title */}
-            <Title setMessages={setMessages} />
+        <div className="flex justify-between items-center w-full p-4 bg-gray-900 text-white font-bold shadow">
+            <div className="italic">FluentBridge</div>
+            <button
+                onClick={resetConversation}
+                className={
+                    "transition-all duration-300 text-blue-300 hover:text-pink-500 " +
+                    (isResetting && "animate-pulse")
+                }
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
 
-            <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
-                {/* Conversation */}
-                <div className="mt-5 px-5">
-                    {messages?.map((audio, index) => {
-                        return (
-                            <div
-                                key={index + audio.sender}
-                                className={
-                                    "flex flex-col " +
-                                    (audio.sender === "rachel" && "flex items-end")
-                                }
-                            >
-                                {/* Sender */}
-                                <div className="mt-4 ">
-                                    <p
-                                        className={
-                                            audio.sender === "rachel"
-                                                ? "text-right mr-2 italic text-green-500"
-                                                : "ml-2 italic text-blue-500"
-                                        }
-                                    >
-                                        {audio.sender}
-                                    </p>
 
-                                    {/* Message */}
-                                    <audio
-                                        src={audio.blobUrl}
-                                        className="appearance-none"
-                                        controls
-                                    />
-                                </div>
-                            </div>
-                        );
-                    })}
-
-                    {messages.length === 0 && !isLoading && (
-                        <div className="text-center font-light italic mt-10">
-                            Send FluentBridge a message...
-                        </div>
-                    )}
-
-                    {isLoading && (
-                        <div className="text-center font-light italic mt-10 animate-pulse">
-                            Gimme a few seconds...
-                        </div>
-                    )}
-                </div>
-
-                {/* Recorder */}
-                <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
-                    <div className="flex justify-center items-center w-full">
-                        <div>
-                            <RecordMessage handleStop={handleStop} />
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                </svg>
+            </button>
         </div>
     );
-};
+}
 
-export default Controller;
+export default Title;
