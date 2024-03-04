071a41a4eb4058daa5b83218a0c89dbcf1cec876
nuwafazley <azleynuwaf2002@gmail.com>
2024-03-05 00:10:36 +0530

    contact-update-01

--- FluentBridge-Client/src/components/ContactPage/ContactUs.jsx
+++ FluentBridge-Client/src/components/ContactPage/ContactUs.jsx
@@ -1,7 +1,34 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react"
-
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import firebaseDB from "./firebase";
+import "./App.scss";
 const ContactUs = () => {
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+    });
+
+    const { name, email, subject, message } = state;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !subject || !message) {
+            toast.error("Please provide value in each input field");
+        } else {
+            firebaseDB.child("contacts").push(state);
+            setState({ name: "", email: "", subject: "", message: "" });
+            toast.success("Form Submitted Successfully");
+        }
+    };
+
+    const handleInputChange = (e) => {
+        let { name, value } = e.target;
+        setState({ ...state, [name]: value });
+    };
     return (
         <>
             <section className='hero'>
