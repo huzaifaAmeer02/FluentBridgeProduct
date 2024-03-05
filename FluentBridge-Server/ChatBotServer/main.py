3071b8bf1001e76d86423ae534595b9378c072b3
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 08:09:24 +0530

    libraries and main.py added

---
+++ FluentBridge-Server/ChatBotServer/main.py
@@ -0,0 +1,10 @@
+
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+
+
+app = FastAPI()
