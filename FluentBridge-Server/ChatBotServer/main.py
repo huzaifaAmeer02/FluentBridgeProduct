defb7b16d02f2ad0c912be9f556b462e458a63bc
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 08:20:59 +0530

    import custom functions to main.py

--- FluentBridge-Server/ChatBotServer/main.py
+++ FluentBridge-Server/ChatBotServer/main.py
@@ -6,5 +6,10 @@ from decouple import config
 import openai
 
 
+# Custome function imports
+from functions.openai_request import convert_audio_to_text, get_chat_response
+from functions.database import store_mesages, recet_messages
+from functions.text_to_speech import convvert_text_to_speech
+
 
 app = FastAPI()
