f4fa551c7cdb5af89cabeca799bac53c2af586a2
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 09:57:27 +0530

    mongo db connected

--- FluentBridge-Server/SignupBackend/index.js
+++ FluentBridge-Server/SignupBackend/index.js
@@ -18,3 +18,32 @@ const bodyParser = require('body-parser');
 const port = process.env.SERVER_PORT | 3000;
 const app = express();
 app.use(cors())
+
+const userRoute = require('./route/UserRoute');
+const customerRoute = require('./route/CustomerRoute');
+
+//-----------------------
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+try{
+    mongoose.connect('mongodb://127.0.0.1:27017/fluentBridge');
+    app.listen(port,()=>{
+        console.log(`server Started & running on port ${port}`);
+    })
+}catch (e){
+    console.log(e);
+}
+
+app.get('/test-api',(req,resp)=>{
+    return resp.json({'message':'Server Started!'})
+})
+
+//------------
+app.use('/api/v1/users',userRoute);
+
+
+app.use('/api/v1/customers',customerRoute);
