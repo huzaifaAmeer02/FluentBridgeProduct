71097debdb5fd75959dcf8337a2e5139bef7fa18
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 09:12:03 +0530

    user Route updated

--- FluentBridge-Server/SignupBackend/route/UserRoute.js
+++ FluentBridge-Server/SignupBackend/route/UserRoute.js
@@ -3,3 +3,27 @@ const bcrypt= require('bcrypt');
 const salt=10;
 const nodemailer= require('nodemailer');
 const jsonwebtoken =require('jsonwebtoken');
+
+const signup = (req,resp) => {
+
+    userSchema.findOne({'email':req.body.email}).then(result=>{
+        if(result==null){
+            bcrypt.hash(req.body.password,salt,function (err,hash) {
+                if (err){
+                    return resp.status(500).json(err);
+                }
+                const user = new userSchema({
+                    fullName:req.body.fullName,
+                    password:hash,
+                    email:req.body.email,
+                    activeState:true
+                });
+
+
+        }else{
+            return resp.status(409).json({'error':'already exists!'});
+        }
+    })
+
+
+}
