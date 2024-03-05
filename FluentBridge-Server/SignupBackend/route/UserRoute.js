78dd3ee348319c4a578d3bce6b14f3a7cb9d5362
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 09:12:51 +0530

    user Route updated and nodemailer sendeing email option added

--- FluentBridge-Server/SignupBackend/route/UserRoute.js
+++ FluentBridge-Server/SignupBackend/route/UserRoute.js
@@ -18,6 +18,32 @@ const signup = (req,resp) => {
                     email:req.body.email,
                     activeState:true
                 });
+                const transporter= nodemailer.createTransport({
+                    service:'gmail',
+                    auth:{
+                        user:'testdevstackemail@gmail.com',
+                        pass:'jxdo sqxg szag keuu',
+                    }
+                });
+
+                const mailOption={
+                    from:'faslan2002rizni@gmail.com',
+                    to:req.body.email,
+                    subject:'New Account Creation',
+                    text:'You have Created Your Account!'
+                }
+                transporter.sendMail(mailOption, function (error, info) {
+                    if (error){
+                        return resp.status(500).json({'error':error});
+                    }else{
+                        user.save().then(saveResponse=>{
+                            return resp.status(201).json({'message':'Saved!'});
+                        }).catch(error=>{
+                            return resp.status(500).json(error);
+                        });
+                    }
+                })
+            })
 
 
         }else{
