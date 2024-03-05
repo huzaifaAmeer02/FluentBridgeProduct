b22c42ee630f252133aa962de0c2fe2a5e980e7b
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 08:24:01 +0530

    middleware and fastapi end points added and get audio from eleven  labs

--- FluentBridge-Server/ChatBotServer/main.py
+++ FluentBridge-Server/ChatBotServer/main.py
@@ -24,3 +24,58 @@ Origins = [
     "http://localhost:3000"
 ]
 # CORS - Origins
+
+
+# CORS - middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=Origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health")
+async def check_health():
+    return {"message": "healthy"}
+
+
+# reset messages
+@app.get("/reset_conversation")
+async def reset_conversation():
+    recet_messages()
+    return {"message": "conversation reset"}
+
+# get audio
+@app.post("/post-audio/")
+async def post_audio(file: UploadFile = File(...)):
+    """ # get saved audio
+    audio_input = open("audio.mp3", "rb") """
+
+    # save file from front end
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    audio_input = open(file.filename, "rb")
+
+    # decode audio
+    message_decoded = convert_audio_to_text(audio_input)
+
+    print(message_decoded)
+
+    # to ensure message decode
+    if not message_decoded:
+        return HTTPException(status_code=400, detail="failed to decode the audio")
+
+    # get chat gpt response
+    chat_response = get_chat_response(message_decoded)
+    print(chat_response)
+
+    if not chat_response:
+        return HTTPException(status_code=400, detail="failed to get chat response")
+
+    # store messages
+    store_mesages(message_decoded, chat_response)
+
+    # Convert chat response to audio
+    audio_output = convvert_text_to_speech(chat_response)
