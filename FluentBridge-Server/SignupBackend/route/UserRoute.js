87f6e46c0665fb6649b2c7b5b857f788ba2c9299
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 09:13:36 +0530

    login function added

--- FluentBridge-Server/SignupBackend/route/UserRoute.js
+++ FluentBridge-Server/SignupBackend/route/UserRoute.js
@@ -53,3 +53,30 @@ const signup = (req,resp) => {
 
 
 }
+const login = (req,resp) => {
+    userSchema.findOne({'email':req.body.email}).then(selectedUser=>{
+        if (selectedUser!==null){
+            bcrypt.compare(req.body.password, selectedUser.password, function(err, result) {
+                if (err){
+                    return resp.status(500).json({'message':'internal server error'});
+                }
+
+                if(result){
+                    const payload={
+                        email:selectedUser.email
+                    }
+
+                    const secretKey=process.env.SECRET_KEY;
+                    const expiresIn='24h';
+
+                    const token = jsonwebtoken.sign(payload,secretKey,{expiresIn});
+                    return resp.status(200).json(token);
+                }else{
+                    return resp.status(401).json({'message':'Password is incorrect!'});
+                }
+            });
+        }else{
+            return resp.status(404).json({'message':'not found!'});
+        }
+    });
+}
