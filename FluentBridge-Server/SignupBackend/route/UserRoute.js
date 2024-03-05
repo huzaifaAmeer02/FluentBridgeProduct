afe6da5d7beb922580cc2f83da25022e5627279e
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 09:09:54 +0530

    user Route packages imported

---
+++ FluentBridge-Server/SignupBackend/route/UserRoute.js
@@ -0,0 +1,5 @@
+const userSchema= require('../model/UserSchema');
+const bcrypt= require('bcrypt');
+const salt=10;
+const nodemailer= require('nodemailer');
+const jsonwebtoken =require('jsonwebtoken');
