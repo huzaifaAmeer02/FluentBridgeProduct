5673bf14a7d69e084aa18c1a17b19adcf041ef25
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-05 09:15:41 +0530

    functions changed to user route to user controller

---
+++ FluentBridge-Server/SignupBackend/controller/UserController.js
@@ -0,0 +1,85 @@
+const userSchema= require('../model/UserSchema');
+const bcrypt= require('bcrypt');
+const salt=10;
+const nodemailer= require('nodemailer');
+const jsonwebtoken =require('jsonwebtoken');
+
+const signup = (req,resp) => {
+
+    userSchema.findOne({'email':req.body.email}).then(result=>{
+        if(result==null){
+            bcrypt.hash(req.body.password,salt,function (err,hash) {
+                if (err){
+                    return resp.status(500).json(err);
+                }
+                const user = new userSchema({
+                    fullName:req.body.fullName,
+                    password:hash,
+                    email:req.body.email,
+                    activeState:true
+                });
+                const transporter= nodemailer.createTransport({
+                    service:'gmail',
+                    auth:{
+                        user:'testdevstackemail@gmail.com',
+                        pass:'jxdo sqxg szag keuu',
+                    }
+                });
+
+                const mailOption={
+                    from:'faslan2002rizni@gmail.com',
+                    to:req.body.email,
+                    subject:'New Account Creation',
+                    text:'You have Created Your Account!'
+                }
+                transporter.sendMail(mailOption, function (error, info) {
+                    if (error){
+                        return resp.status(500).json({'error':error});
+                    }else{
+                        user.save().then(saveResponse=>{
+                            return resp.status(201).json({'message':'Saved!'});
+                        }).catch(error=>{
+                            return resp.status(500).json(error);
+                        });
+                    }
+                })
+            })
+
+
+        }else{
+            return resp.status(409).json({'error':'already exists!'});
+        }
+    })
+
+
+}
+const login = (req,resp) => {
+    userSchema.findOne({'email':req.body.email}).then(selectedUser=>{
+        if (selectedUser!==null){
+            bcrypt.compare(req.body.password, selectedUser.password, function(err, result) {
+                if (err){
+                    return resp.status(500).json({'message':'internal server error'});
+                }
+
+                if(result){
+                    const payload={
+                        email:selectedUser.email
+                    }
+
+                    const secretKey=process.env.SECRET_KEY;
+                    const expiresIn='24h';
+
+                    const token = jsonwebtoken.sign(payload,secretKey,{expiresIn});
+                    return resp.status(200).json(token);
+                }else{
+                    return resp.status(401).json({'message':'Password is incorrect!'});
+                }
+            });
+        }else{
+            return resp.status(404).json({'message':'not found!'});
+        }
+    });
+}
+module.exports={
+    signup,login
+}
