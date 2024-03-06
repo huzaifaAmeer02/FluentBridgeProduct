71d2b84463cd6b3043be12ff7571d26f6d20baca
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-03-06 14:56:26 +0530

    congiurations-1

--- FluentBridge-Client/vite.config.js
+++ FluentBridge-Client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sass from 'vite-plugin-sass';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), sass()],
+  resolve: {
+    alias: {
+      'react-toastify': 'react-toastify',
+    },
+  },
+});
