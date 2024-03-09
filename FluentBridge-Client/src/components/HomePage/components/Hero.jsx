b28862d2ea17355fa871ab8f64e24ab5da79f584
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-03-09 12:05:15 +0530

    home-update11

--- FluentBridge-Client/src/components/HomePage/components/Hero.jsx
+++ FluentBridge-Client/src/components/HomePage/components/Hero.jsx
@@ -8,10 +8,11 @@ const Hero = () => {
     return (
         <div className='text-white mt-16' style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
             <div className='max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <h1 className='md:text-7xl text-white sm:text-6xl text-4xl font-bold md:py-6 bg-black-500 opacity-80 rounded-3xl'>
-                    Welcome to our English Learning Platform
+                <h1 className='md:text-7xl text-black-900 sm:text-6xl text-4xl font-bold md:py-6 bg-black-100 opacity-80 rounded-3xl'>
+                    Welcome to FluentBridge
                 </h1>
-                <p className='md:text-2xl text-xl font-bold text-white'>We help you improve your English skills in speaking, writing, listening, and vocabulary.</p>
+                <h5 className='text-lg my-4 bg-gray-800 text-white rounded-2xl py-3 hover:bg-gray-600 hover:text-white'>We help you improve your English skills in speaking, writing, listening, and vocabulary.</h5>
+                <p className='md:text-2xl text-xl font-bold text-whi'>Explore and Learn</p>
                 <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
             </div>
         </div>
