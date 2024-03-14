b082cd17428c85b23cc6ec3c9b0ac034dc98e84c
Muhammadu Ishrath <ishrathmuhammadu@gmail.com>
2024-03-14 13:15:46 +0530

    index.js is added with constant information like tabs in navi  bar

---
+++ FluentBridge-Client/src/components/PurpleThemedLandingPage/constants/index.js
@@ -0,0 +1,229 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  roadmap5,
+  Huzaifa,
+  Nuwaf,
+  Asani,
+  Faslan,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Home",
+    url: "#features",
+  },
+  {
+    id: "1",
+    title:  "Activities",
+    url: "#pricing",
+  },
+  {
+    id: "2",
+    title:  "About US",
+    url: "#how-to-use",
+  },
+  {
+    id: "3",
+    title: "Contact US",
+    url: "#roadmap",
+  },
+  {
+    id: "4",
+    title: "New account",
+    url: "#signup",
+    onlyMobile: true,
+  },
+  {
+    id: "5",
+    title: "Sign in",
+    url: "#login",
+    onlyMobile: true,
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Photo generating",
+  "Photo enhance",
+  "Seamless Integration",
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const roadmap = [
+    {
+      id: "0",
+      title: "Huzaifa Ameer",
+      text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+      date: "CEO",
+      imageUrl: Huzaifa,
+      colorful: true,
+    },
+    {
+      id: "1",
+      title: "Asani Amarasinghe",
+      text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+      date: "CTO",
+      imageUrl: Asani,
+      colorful: true,
+    },
+    {
+      id: "2",
+      title: "Faslan Rizni",
+      text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+      date: "CIO",
+      imageUrl: Faslan,
+      colorful: true,
+    },
+    {
+      id: "3",
+      title: "Nuwaf Azley",
+      text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+      date: "Tech Lead",
+      imageUrl: Nuwaf,
+      colorful: true,
+    },
+    {
+      id: "4",
+      title: "Muhammadu Ishrath",
+      text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+      date: "Designer",
+      imageUrl: roadmap5,
+      colorful: true,
+    },
+];
+
+export const collabText =
+  "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.";
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Seamless Integration",
+    text: collabText,
+  },
+  {
+    id: "1",
+    title: "Smart Automation",
+  },
+  {
+    id: "2",
+    title: "Top-notch Security",
+  },
+];
+
+export const collabApps = [
+  {
+    id: "0",
+    title: "Figma",
+    icon: figma,
+    width: 26,
+    height: 36,
+  },
+  {
+    id: "1",
+    title: "Notion",
+    icon: notion,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: discord,
+    width: 36,
+    height: 28,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 34,
+    height: 35,
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: photoshop,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: protopie,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: framer,
+    width: 26,
+    height: 34,
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+  },
+];
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Basic",
+    description: "Listening, Reading , Writing,Speaking",
+    price: "0",
+    features: [
+      "Priority support to solve issues quickly",
+      "Priority support to solve issues quickly",
+      "Priority support to solve issues quickly",
