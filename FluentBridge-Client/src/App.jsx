843bb7312b29da0b316caf0936de6f3f42661a98
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-17 01:59:58 +0530

    updated user profile

--- FluentBridge-Client/src/App.jsx
+++ FluentBridge-Client/src/App.jsx
@@ -25,7 +25,7 @@ import Signup from "./components/LandingPage/Signup.jsx";
 import Speaking from "./components/SpeakingPage/Speaking.jsx";
 import Pronunciation from "./components/SpeakingPage/prounciation.jsx";
 import WritingActivity from "./components/WritingActivity/WritingActivity.jsx";
-// import User from "./components/user/App";
+import User from "./components/user/Profile";
 
 
 
@@ -60,7 +60,7 @@ function App() {
                     <Route path='/speaking' element={<Speaking/>}/>
                     <Route path='/pronunciation' element={<Pronunciation/>}/>
                     <Route path='/writingactivity' element={<WritingActivity/>}/>
-                    {/*<Route path='/user' element={<User/>}/>*/}
+                    <Route path='/user' element={<User/>}/>
                 </Routes>
             </Router>
         </>
--- FluentBridge-Client/src/components/user/App.jsx
+++ FluentBridge-Client/src/components/user/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Profile from '../user/Profile';
-
-const App = () => {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/profile">
-                        <Profile />
-                    </Route>
-                    {/* Add more routes as needed */}
-                </Switch>
-            </div>
-        </Router>
-    );
-};
-
-export default App;
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Profile from '../user/Profile';
+//
+// const App = () => {
+//     return (
+//         <Router>
+//             <div>
+//                 <Switch>
+//                     <Route path="/profile">
+//                         <Profile />
+//                     </Route>
+//                     {/* Add more routes as needed */}
+//                 </Switch>
+//             </div>
+//         </Router>
+//     );
+// };
+//
+// export default App;
