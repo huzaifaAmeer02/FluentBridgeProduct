c5897a445f33fcc1ea03b476080b0ddca61adde5
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-17 02:26:49 +0530

    updated to add profile image

--- FluentBridge-Server/SignupBackend/model/UserSchema.js
+++ FluentBridge-Server/SignupBackend/model/UserSchema.js
@@ -1,24 +1,21 @@
-const mongoose= require('mongoose');
-const UserSchema =
-    new mongoose.Schema({
-        fullName:{
-            type:String,
-            required:true
-        },
-        username: String,
-        email:{
-            type:String,
-            required:true
-        },
-        password:{
-            type:String,
-            required:true
-        },
-        birthdate: Date,
-        address: String,
-        activeState:{
-            type:Boolean,
-            required:true
-        },
-    });
-module.exports = mongoose.model('user',UserSchema);
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    activeState: {
+        type: Boolean,
+        required: true
+    },
+    profileImage: {
+        type: String // Store the URL of the profile image
+    }
+});
