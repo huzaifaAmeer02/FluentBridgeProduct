fbf0b1a3f2cc688358c2be01d916dea0ce8c5f86
nuwafazley <azleynuwaf2002@gmail.com>
2024-03-16 15:08:30 +0530

    update_videoPage

--- FluentBridge-Client/src/components/ListeningActivity/VideoPage.jsx
+++ FluentBridge-Client/src/components/ListeningActivity/VideoPage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ReactPlayer from "react-player";
 
- //eslint-disable-next-line react/prop-types
-const VideoPage = ({ onClose, videoUrl }) => {
+// eslint-disable-next-line react/prop-types
+const VideoPage = ({ onClose, videoUrl, quizLink }) => {
     return (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
             <div className="bg-purple-200 p-8 rounded-lg relative">
@@ -23,7 +23,8 @@ const VideoPage = ({ onClose, videoUrl }) => {
                     height="auto"
                     className="w-full max-w-screen-lg max-h-screen-3/4 mx-auto mt-8"
                 />
-                <Link to="/listeningquiz" className="block text-center mt-4 bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-950 transition duration-300">Take Quiz</Link>
+                {/* Use the passed quizLink prop for the quiz */}
+                <Link to={quizLink} className="block text-center mt-4 bg-purple-700 text-white px-4 py-2 rounded-md hover:bg-purple-950 transition duration-300">Take Quiz</Link>
             </div>
         </div>
     );
