0fa48c8f34dd83f68f7e990860e4a1be34a1194d
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-03-18 00:23:57 +0530

    u-23

--- FluentBridge-Client/src/components/GradingAssesment/ResultsTables.jsx
+++ FluentBridge-Client/src/components/GradingAssesment/ResultsTables.jsx
@@ -22,31 +22,31 @@ export default function ResultsTables() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
             >
-                <h3 className="text-xl font-semibold mb-4">Grading Assesment Rankings</h3>
+                <h3 className="text-xl font-semibold mb-4 text-purple-950">Grading Assesment Rankings</h3>
                 <div className="overflow-x-auto">
                     <table className="min-w-full divide-y divide-purple-200">
                         <thead className="bg-gray-50 sticky top-0">
                             <tr>
-                                <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th className="px-3 py-2 text-left text-xs font-medium text-purple-900 uppercase tracking-wider">
                                     Rank
                                 </th>
-                                <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th className="px-3 py-2 text-left text-xs font-medium text-purple-900 uppercase tracking-wider">
                                     Username
                                 </th>
-                                <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th className="px-3 py-2 text-left text-xs font-medium text-purple-900 uppercase tracking-wider">
                                     Attempts
                                 </th>
-                                <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th className="px-3 py-2 text-left text-xs font-medium text-purple-900 uppercase tracking-wider">
                                     Earned Points
                                 </th>
-                                <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th className="px-3 py-2 text-left text-xs font-medium text-purple-900 uppercase tracking-wider">
                                     Result
                                 </th>
                             </tr>
                         </thead>
                         <tbody className="bg-white divide-y divide-gray-200">
                             {attemptData.map((attempt, index) => (
-                                <tr key={index} className={index === 0 ? 'bg-yellow-200' : ''}>
+                                <tr key={index} className={index === 0 ? 'bg-[#862E70]' : ''}>
                                     <td className="px-4 py-3 whitespace-nowrap sm:px-1 sm:py-2 md:px-3 md:py-3">{index + 1}</td>
                                     <td className="px-4 py-3 whitespace-nowrap sm:px-1 sm:py-2 md:px-3 md:py-3">{attempt.userId}</td>
                                     <td className="px-4 py-3 whitespace-nowrap sm:px-1 sm:py-2 md:px-3 md:py-3">{attempt.attempts}</td>
