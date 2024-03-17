122b35d58e43981763a7384285495bf77a36abda
AAAMAmarasinghe <asaniamarasingha@gmail.com>
2024-03-17 20:13:36 +0530

    newWordListDB

---
+++ FluentBridge-Server/saveMyLIst/model/WordList.js
@@ -0,0 +1,10 @@
+// WordList.js
+
+const mongoose = require('mongoose');
+
+const WordListSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    words: [{ type: String }],
+});
+
+module.exports = mongoose.model('WordList', WordListSchema);
