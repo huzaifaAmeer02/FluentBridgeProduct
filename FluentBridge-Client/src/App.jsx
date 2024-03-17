279c5f8e78d3a0f202dac8094ebc0e229d7afd6a
Muhammadu Ishrath <ishrathmuhammadu@gmail.com>
2024-03-17 14:40:04 +0530

    NewOne become the Home page

--- FluentBridge-Client/src/App.jsx
+++ FluentBridge-Client/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<LandingPage />} />
+                    <Route path='/' element={<NewOne />} />
                     <Route path='/home' element={<Home />} />
                     <Route path='/about' element={<About />} />
                     <Route path='/activities' element={<Activity />} />
@@ -63,7 +63,7 @@ function App() {
                     <Route path='/speaking' element={<Speaking/>}/>
                     <Route path='/pronunciation' element={<Pronunciation/>}/>
                     <Route path='/writingactivity' element={<WritingActivity/>}/>
-                        <Route path='/NewOne' element={<NewOne/>}/>
+                        {/* <Route path='/NewOne' element={<NewOne/>}/> */}
                 </Routes>
             </Router>
         </>
