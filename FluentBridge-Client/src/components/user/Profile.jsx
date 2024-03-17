5ef4d42d836392cf2c971c53e156e4f6ac3a3584
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-03-18 01:24:11 +0530

    updated user profile 3

--- FluentBridge-Client/src/components/user/Profile.jsx
+++ FluentBridge-Client/src/components/user/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UpdateProfileForm from './UpdateProfileForm';
 import ChangePasswordForm from './ChangePasswordForm';
+import CoverImage from './CoverImage'; // Import the CoverImage component
 
 const Profile = () => {
     const [userData, setUserData] = useState(null);
@@ -18,23 +19,29 @@ const Profile = () => {
     }, []);
 
     return (
-        <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md mt-10">
-            {userData && (
-                <div>
-                    <h2 className="text-2xl font-bold mb-4">User Profile</h2>
-                    <div className="mb-4">
-                        <p className="font-semibold">Name:</p>
-                        <p>{userData.fullName}</p>
+        <>
+            <CoverImage /> {/* Add the CoverImage component */}
+            <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md mt-10">
+
+                {userData && (
+                    <div>
+
+                        <h2 className="text-2xl font-bold mb-4">User Profile</h2>
+                        <div className="mb-4">
+                            <p className="font-semibold">Name:</p>
+                            <p>{userData.fullName}</p>
+                        </div>
+                        <div className="mb-4">
+                            <p className="font-semibold">Email:</p>
+                            <p>{userData.email}</p>
+                        </div>
+                        <UpdateProfileForm userData={userData} />
+                        <ChangePasswordForm />
                     </div>
-                    <div className="mb-4">
-                        <p className="font-semibold">Email:</p>
-                        <p>{userData.email}</p>
-                    </div>
-                    <UpdateProfileForm userData={userData} />
-                    <ChangePasswordForm />
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </>
+
     );
 };
 
