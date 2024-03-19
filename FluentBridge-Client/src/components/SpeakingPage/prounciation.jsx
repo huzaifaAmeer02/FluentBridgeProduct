259c285f81875844d62af64ccd236f2bb76ce838
nuwafazley <azleynuwaf2002@gmail.com>
2024-03-19 18:06:32 +0530

    Update prounciation.jsx

--- FluentBridge-Client/src/components/SpeakingPage/prounciation.jsx
+++ FluentBridge-Client/src/components/SpeakingPage/prounciation.jsx
@@ -26,10 +26,10 @@ const Pronunciation = () => {
     }
 
     return (
-        <div className="bg-gray-400 min-h-screen flex flex-col justify-center items-center">
+        <div className="bg-gradient-to-br from-purple-900 to-purple-700 min-h-screen flex flex-col justify-center items-center">
             <Link
                 to="/speakingmainpanel"
-                className="absolute left-4 top-4 p-2 rounded-lg bg-white text-blue-500 font-bold hover:text-blue-700 transition duration-300 ease-in-out"
+                className="absolute left-4 top-4 p-2 rounded-lg bg-white text-purple-700 font-bold hover:text-blue-700 transition duration-300 ease-in-out"
             >
                 <IoIosArrowBack />
             </Link>
@@ -39,8 +39,8 @@ const Pronunciation = () => {
             </div>
             <div className="mt-4 text-center mx-auto max-w-lg">
                 <h1 className="text-3xl font-bold mb-6 text-white">{isTranslated ? 'FluentBridge චතුර මට්ටමේ අනාවැකිකරු වෙත සාදරයෙන් පිළිගනිමු' : 'Welcome to FluentBridge Fluency Level Predictor'}</h1>
-                <p className="mb-6 text-lg text-gray-700">{isTranslated ? 'FluentBridge චතුර මට්ටමේ පුරෝකථනය භාවිතා කිරීම සඳහා:' : 'To use the FluentBridge fluency level predictor:'}</p>
-                <ol className="text-left mb-6 bg-gray-600 p-8 rounded-2xl mx-4">
+                <p className="mb-6 text-lg text-gray-200">{isTranslated ? 'FluentBridge චතුර මට්ටමේ පුරෝකථනය භාවිතා කිරීම සඳහා:' : 'To use the FluentBridge fluency level predictor:'}</p>
+                <ol className="text-left mb-6 bg-gray-800 p-8 rounded-2xl mx-4">
                     <li className="mb-2"><strong>{isTranslated ? 'ක්ලික් කරන්න' : 'Click'}</strong> {isTranslated ? '- මෙම "Predict My Fluency Level" වෙත ක්ලික් කරන්න.' : 'on the "Predict My Fluency Level" button below.'}</li>
                     <li className="mb-2"><strong>{isTranslated ? 'කතා කරන්න' : 'Speak'}</strong> {isTranslated ? '- මෙම බොත්තම ක්ලික් කර පසුබිමට කතා කරන්න.' : 'into the microphone after clicking the button.'}</li>
                     <li className="mb-2"><strong>{isTranslated ? 'රැඳී සිටින්න' : 'Wait'}</strong> {isTranslated ? '- විශ්ලේෂණ ප්රතිඵල පෙනී සිටීම සඳහා.' : 'for the analysis results to appear.'}</li>
@@ -49,7 +49,7 @@ const Pronunciation = () => {
                 <div>
                     <button
                         onClick={() => window.location.href = "https://your-url.com"}
-                        className="mb-4 bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-8 rounded-md shadow-md transition duration-300 ease-in-out"
+                        className="mb-4 bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-8 rounded-md shadow-md transition duration-300 ease-in-out"
                     >
                         {isTranslated ? 'මගේ චතුර මට්ටම පුරෝකථනය කරන්න' : 'Predict My Fluency Level'}
                     </button>
@@ -57,7 +57,7 @@ const Pronunciation = () => {
                 <div>
                     <button
                         onClick={toggleTranslation}
-                        className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-md shadow-md transition duration-300 ease-in-out"
+                        className="mb-4 bg-blue-600 hover:bg-blue-800 text-white font-bold py-3 px-8 rounded-md shadow-md transition duration-300 ease-in-out"
                     >
                         {isTranslated ? 'Translate to English' : 'සිංහල භාෂාවට පරිවර්තනය කරන්න'}
                     </button>
@@ -65,6 +65,7 @@ const Pronunciation = () => {
             </div>
         </div>
     );
+
 };
 
 export default Pronunciation;
