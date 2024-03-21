9df7b45b87bc5b79cbff60cde1e7c6f786abe8c0
Ishrath <130290533+MuhammaduIshrath@users.noreply.github.com>
2024-03-21 06:17:14 +0530

    initially assigning the recording variable to false

--- FluentBridge-Model/static/record.js
+++ FluentBridge-Model/static/record.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const recordButton = document.getElementById("recordButton");
     const recordingStatus = document.getElementById("recordingStatus");
+
+let isRecording = false;
+    let mediaRecorder;
+    let audioChunks = [];
+
