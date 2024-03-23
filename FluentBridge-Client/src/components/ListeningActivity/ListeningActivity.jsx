62d8b4694f4fed2900b98b75c1a552cdc542c609
nuwafazley <azleynuwaf2002@gmail.com>
2024-03-23 16:32:44 +0530

    Update ListeningActivity.jsx

--- FluentBridge-Client/src/components/ListeningActivity/ListeningActivity.jsx
+++ FluentBridge-Client/src/components/ListeningActivity/ListeningActivity.jsx
@@ -1,7 +1,9 @@
-import React, {useEffect, useState} from "react";
+
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import VideoPage from "./VideoPage.jsx"; // Importing the VideoPage component
-import {IoIosArrowBack} from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 import LoadingPage from "../LoadingPage/LoadingPage.jsx";
 
 const ListeningActivity = () => {
@@ -10,7 +12,7 @@ const ListeningActivity = () => {
         {
             title: "Labour",
             //image:"https://img.freepik.com/free-vector/construction-illustration-with-foreman-giving-instructions-team-builders-dressed-uniform-helmets_1284-29082.jpg?t=st=1710990122~exp=1710993722~hmac=a99f477327206f96abe89a0f6424cca15840d1bbb94719a7215a0a4a2fddc5e1&w=996",
-           image:"https://img.freepik.com/free-vector/kids-overalls-helmets-building-house-happy-male-female-constructors-laying-bricks-using-construction-tools-flat-vector-illustration_74855-18390.jpg?t=st=1710088864~exp=1710092464~hmac=b06b72e32bae7899de2b18c94676a02cf6e4af4459f85d7d7de214c6a0757b78&w=900",
+            image:"https://img.freepik.com/free-vector/kids-overalls-helmets-building-house-happy-male-female-constructors-laying-bricks-using-construction-tools-flat-vector-illustration_74855-18390.jpg?t=st=1710088864~exp=1710092464~hmac=b06b72e32bae7899de2b18c94676a02cf6e4af4459f85d7d7de214c6a0757b78&w=900",
             //image: "https://img.freepik.com/free-photo/construction-workers-sunset_53876-138180.jpg?w=900&t=st=1707675337~exp=1707675937~hmac=88653ffa23aecd19c4753b729f43b7e73dc70638b03813eab70d37a3fa4433a4",
             link: "/Labour",
             videoUrl: "https://youtu.be/dImiR3Sr8Wo",
@@ -82,15 +84,7 @@ const ListeningActivity = () => {
     ];
 
     const [selectedJob, setSelectedJob] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [showInstructions, setShowInstructions] = useState(false); // State to track instructions panel visibility
-    const [translatedInstructions, setTranslatedInstructions] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000); // Simulating a 2-second delay for demonstration purposes
-    }, []);
+    const [loading] = useState(true);
 
     const handleJobClick = (job) => {
         setSelectedJob(job);
@@ -99,101 +93,24 @@ const ListeningActivity = () => {
     const handleClosePopup = () => {
         setSelectedJob(null);
     };
-    const toggleInstructions = () => {
-        setShowInstructions(!showInstructions);
-    };
-    const translateInstructions = () => {
-        // Placeholder translation logic, replace with your actual translation logic
-        // For demonstration purposes, we'll just toggle between English and Sinhala
-        if (translatedInstructions === null) {
-            setTranslatedInstructions({
-                objective: "අරමුණ",
-                procedure: "පටිපාටිය",
-                answeringQuestions: "ප්‍රශ්න වලට පිළිතුරු දීම",
-                completingActivity: "ක්රියාකාරකම් සම්පූර්ණ කිරීම",
-                note: "සටහන",
-                oli1:""
-            });
-        } else {
-            setTranslatedInstructions(null);
-        }
-    };
-
 
     return (
-        < >
+        <>
             {loading ? (
                 <LoadingPage />
             ) : (
-                <section className=" py-8 relative bg-purple-100" >
+                <section className="py-8 relative bg-purple-100">
+                    {/* Add a back button */}
                     <Link
                         to="/activities"
                         className="back-to-activities flex items-center text-purple-500 font-bold hover:text-purple-700 transition duration-300 ease-in-out rounded-lg p-2 bg-white absolute left-4 top-4"
                     >
                         <IoIosArrowBack />
                     </Link>
-                    <h2 className="text-3xl  font-bold text-center mb-8 text-purple-950">
+                    <h2 className="text-3xl font-bold text-center mb-8 text-purple-950">
                         Listening Activity
                     </h2>
-                    <button
-                        className="bg-purple-600 hover:bg-purple-800 text-white py-2 px-4 rounded mb-8 mx-auto block"
-                        onClick={toggleInstructions}
-                    >
-                        {showInstructions ? "Hide Instructions" : "Show Instructions"}
-                    </button>
-                    {showInstructions && (
-                        <div className="bg-white p-4 rounded-3xl shadow-lg mb-4 mx-4">
-                            <h3 className="text-xl font-bold mb-2 text-purple-950">Instructions:</h3>
-                            <p className="text-purple-800 bg-purple-200 rounded-3xl p-10 text-justify">
-                                {/* Instruction details */}
-                                <ol className="list-decimal ml-6">
-                                    <li><strong>{translatedInstructions ? translatedInstructions.objective : "Objective"} :</strong>
-                                        <ul className="list-disc ml-6">
-                                            <li>{translatedInstructions ? "මෙම සවන්දීමේ කාර්යයේ පරමාර්ථය වන්නේ වීඩියෝවක් නැරඹීම සහ සවන්දීම සහ එහි සාකච්ඡා කෙරෙන ප්‍රධාන සහ වැදගත් කරුණු හඳුනා ගැනීමයි." : "The objective of this listening activity is to watch a video and identify the main and important points discussed in it."}</li>
-                                        </ul>
-                                    </li>
-
-                                    <li className="mt-4"><strong>{translatedInstructions ? translatedInstructions.procedure : "Procedure"} :</strong>
-                                        <ul className="list-disc ml-6">
-                                            <li>{translatedInstructions ? "වීඩියෝව නැරඹීම ආරම්භ කිරීමට 'Play Video' බොත්තම ක්ලික් කරන්න." : "Click on the 'Play Video' button to start watching the video."}</li>
-                                            <li>{translatedInstructions ? "වීඩියෝව නරඹන අතරතුර, ඉදිරිපත් කරන අන්තර්ගතය කෙරෙහි දැඩි අවධානයක් යොමු කරන්න." : "While watching the video, pay close attention to the content being presented."}</li>
-                                            <li>{translatedInstructions ? "වීඩියෝවේ සඳහන් කර ඇති ප්‍රධාන මාතෘකා, ප්‍රධාන අදහස් සහ ඕනෑම වැදගත් තොරතුරු හඳුනා ගන්න." : "Identify the main topics, key ideas, and any important information mentioned in the video."}</li>
-                                            <li>{translatedInstructions ? "ඔබට අවශ්‍ය නම් ඔබට කඩදාසි කැබැල්ලක සටහන් ගත හැක, නමුත් සෑම ප්‍රශ්නයකටම තත්පර 30 ක කාල සීමාවක් ඇති බව මතක තබා ගන්න, එබැවින් ඔබේ කාලය ඵලදායී ලෙස කළමනාකරණය කරන්න." : "You may take notes on a piece of paper if you wish, but remember that each question will have a time limit of 30 seconds, so manage your time effectively."}</li>
-                                        </ul>
-                                    </li>
-
-                                    <li className="mt-4"><strong>{translatedInstructions ? translatedInstructions.answeringQuestions : "Answering Questions"} :</strong>
-                                        <ul className="list-disc ml-6">
-                                            <li>{translatedInstructions ? "වීඩියෝව නැරඹීමෙන් පසු, අන්තර්ගතයට අදාළ ප්රශ්න මාලාවක් ඔබට ඉදිරිපත් කරනු ඇත." : "After watching the video, you will be presented with a series of questions related to the content."}</li>
-                                            <li>{translatedInstructions ? "සෑම ප්‍රශ්නයකටම තත්පර 30 ක කාල සීමාවක් ඇත." : "Each question will have a time limit of 30 seconds."}</li>
-                                            <li>{translatedInstructions ? "එක් එක් ප්‍රශ්නය සඳහා ඔබේ පිළිතුර ප්‍රවේශමෙන් තෝරන්න, තෝරාගත් පසු ඔබේ පිළිතුර වෙනස් කිරීමට අවස්ථාවක් නොලැබේ." : "Select your answer for each question carefully, as there will be no opportunity to change your answer once selected."}</li>
-                                        </ul>
-                                    </li>
-
-                                    <li className="mt-4"><strong>{translatedInstructions ? translatedInstructions.completingActivity : "Completing the Activity"} :</strong>
-                                        <ul className="list-disc ml-6">
-                                            <li>{translatedInstructions ? "ඔබ සියලු ප්‍රශ්නවලට පිළිතුරු දුන් පසු, ක්‍රියාකාරකම සම්පූර්ණ වනු ඇත." : "Once you have answered all the questions, the activity will be complete."}</li>
-                                            <li>{translatedInstructions ? "ඔබේ පිළිතුරුවල නිරවද්‍යතාවය සහ වීඩියෝවෙන් ප්‍රධාන කරුණු හඳුනා ගැනීමට ඔබට ඇති හැකියාව මත පදනම්ව ඔබේ කාර්ය සාධනය ඇගයීමට ලක් කෙරේ." : "Your performance will be evaluated based on the accuracy of your answers and your ability to identify the main points from the video."}</li>
-                                        </ul>
-                                    </li>
-
-                                    <li className="text-red-900 bg-red-100 p-4 mt-4 rounded-3xl"><strong>{translatedInstructions ? translatedInstructions.note : "Note"} :</strong>
-                                        <ul className="list-disc ml-6">
-                                            <li>{translatedInstructions ? "වීඩියෝව නැරඹීමට සහ අන්තර්ගතය කෙරෙහි අවධානය යොමු කිරීමට ඔබට නිස්කලංක පරිසරයක් ඇති බවට සහතික වන්න." : "Ensure that you have a quiet environment to watch the video and concentrate on the content."}</li>
-                                            <li>{translatedInstructions ? "අවධානය වෙනතකට යොමු කිරීමෙන් වළකින්න සහ වීඩියෝවේ ඉදිරිපත් කර ඇති තොරතුරු තේරුම් ගැනීමට අවධානය යොමු කරන්න." : "Avoid distractions and focus on understanding the information presented in the video."}</li>
-                                        </ul>
-                                    </li>
-                                </ol>
-                            </p>
-                            <div className="mt-4 text-center">
-                                <button className="bg-purple-800 hover:bg-purple-900 text-white py-2 px-4 rounded" onClick={translateInstructions}>
-                                    {translatedInstructions ? "English" : "සිංහල"}
-                                </button>
-                            </div>
-                        </div>
-                    )}
-
-                    {/* Mapping through job items */}
+                    {/* Render job items */}
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 px-4">
                         {jobs.map((job, index) => (
                             <div key={index}>
@@ -216,6 +133,7 @@ const ListeningActivity = () => {
                             </div>
                         ))}
                     </div>
+                    {/* Render VideoPage if a job is selected */}
                     {selectedJob && (
                         <VideoPage onClose={handleClosePopup} videoUrl={selectedJob.videoUrl} />
                     )}
