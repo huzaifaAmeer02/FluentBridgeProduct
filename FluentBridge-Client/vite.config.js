daf27ebf24a42892d6e59f457383c684691ba972
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-03-24 13:09:01 +0530

    vite-update

--- FluentBridge-Client/vite.config.js
+++ FluentBridge-Client/vite.config.js
@@ -3,11 +3,18 @@ import react from '@vitejs/plugin-react';
 import sass from 'vite-plugin-sass';
 
 export default defineConfig({
-  plugins: [react(), sass()],
+  build: {
+    rollupOptions: {
+      external: ['@emotion/styled'], // Add @emotion/styled to externalize it
+    },
+  },
+  plugins: [
+    react(), // Keep the react plugin
+    sass(), // Keep the sass plugin
+  ],
   resolve: {
     alias: {
-      'react-toastify': 'react-toastify',
+      'react-toastify': 'react-toastify', // Keep the existing alias
     },
   },
 });
-
